e52129962527e3fe2b5b64445ce34efc95940bc9
MadDevX <eryk.dobrosz@gmail.com>
2020-07-12 21:51:45 +0200

    Add C0 handling for intersections

--- avoCADo/Algebra/NewtonMethod.cs
+++ avoCADo/Algebra/NewtonMethod.cs
@@ -12,7 +12,8 @@ namespace avoCADo.Algebra
     public static class NewtonMethod
     {
         private const int _intersectionPointsLimit = 2000;
-        private const float _pointDistanceEpsilon = 0.001f;
+        private const float _pointDistanceEpsilon = 0.01f;
+        private const float _c0AngleThreshold = (float)Math.PI * 0.45f;
 
         public static IList<Vector4> CalculateIntersectionPoints(IntersectionData data, Vector4 startingPoint, float knotDistance, float epsilon = 0.00001f)
         {
@@ -94,8 +95,8 @@ namespace avoCADo.Algebra
         private static bool IsLooped(IntersectionData data, Vector4 startingPoint, Vector4 secondPoint, Vector4 currentPoint, float knotDistance, float epsilon = 0.5f)
         {
             var eps = knotDistance * epsilon;
-            var stepFirst = StepDistance(data, currentPoint, startingPoint, knotDistance);
-            var stepSecond = StepDistance(data, currentPoint, secondPoint, knotDistance);
+            var stepFirst = StepDistance(data, currentPoint, startingPoint, knotDistance, false, Vector4.Zero);//TODO: check
+            var stepSecond = StepDistance(data, currentPoint, secondPoint, knotDistance , false, Vector4.Zero);//TODO: check
             var distCur = (currentPoint - startingPoint).Length;
             var distSec = (secondPoint - startingPoint).Length;
             return (stepFirst >= -knotDistance - eps && stepFirst <= 0.0f + eps && stepSecond <= -knotDistance + eps && distCur <= distSec);
@@ -108,10 +109,17 @@ namespace avoCADo.Algebra
             var xPrev = x0;
 
             int i = 0;
-            while(NewtonCondition(data, xPrev, xCur, x0, knotDistance, epsilon) && i < maxIterations)
+            bool useCurrentTangent = false;
+            Vector4 xTg = Vector4.Zero;
+            while(NewtonCondition(data, xPrev, xCur, x0, knotDistance, useCurrentTangent, xTg, epsilon) && i < maxIterations)
             {
                 xPrev = xCur;
-                xCur = NewtonIteration(data, xCur, x0, knotDistance);
+                xCur = NewtonIteration(data, xCur, x0, knotDistance, useCurrentTangent, xTg);
+                if(IsSharpC0(data, xCur, x0))
+                {
+                    useCurrentTangent = true;
+                    xTg = xCur;
+                }
                 xCur = ParameterHelper.CorrectUVST(data.p, data.q, xCur);
                 i++;
             }
@@ -119,11 +127,29 @@ namespace avoCADo.Algebra
             return xCur; //TODO: throw exception when no solution was found
         }
 
-        private static bool NewtonCondition(IntersectionData data, Vector4 xPrev, Vector4 xCur, Vector4 x0, float knotDistance, float epsilon)
+        private static bool IsSharpC0(IntersectionData data, Vector4 xCur, Vector4 x0)
         {
+            if (data.p.DifferentPatches(xCur.X, xCur.Y, x0.X, x0.Y) || data.q.DifferentPatches(xCur.Z, xCur.W, x0.Z, x0.W))
+            {
+                var tg1 = ICTangent(data, x0);
+                var tg2 = ICTangent(data, xCur);
+                return Vector3.CalculateAngle(tg1, tg2) > _c0AngleThreshold;
+            }
+            return false;
+        }
+
+        private static bool NewtonCondition(IntersectionData data, Vector4 xPrev, Vector4 xCur, Vector4 x0, float knotDistance, bool useCurrentTangent, Vector4 xTg, float epsilon)
+        {
+            var canGetValue = SurfaceConditions.ParametersInBounds(data, xCur);
+            bool distanceNotOK = true;
+            if (canGetValue)
+            {
+                var pointDiff = data.p.GetVertex(xCur.X, xCur.Y) - data.q.GetVertex(xCur.Z, xCur.W);
+                distanceNotOK = pointDiff.Length > epsilon;
+            }
             return SurfaceConditions.ParametersInBounds(data, xCur) && //TODO : check condition
-                 (Vector3.Dot(data.p.GetVertex(xCur.X, xCur.Y), data.q.GetVertex(xCur.Z, xCur.W)) > epsilon || 
-                 Math.Abs(StepDistance(data, xCur, x0, knotDistance)) > epsilon); //TODO: tweak end condition, use previous approx.
+                 (distanceNotOK || 
+                 Math.Abs(StepDistance(data, xCur, x0, knotDistance, useCurrentTangent, xTg)) > epsilon); //TODO: tweak end condition, use previous approx.
         }
 
         private static bool PointsCloseEnough(IntersectionData data, Vector4 point, float epsilon)
@@ -133,16 +159,16 @@ namespace avoCADo.Algebra
         }
 
 
-        private static Vector4 NewtonIteration(IntersectionData data, Vector4 xCur, Vector4 x0, float knotDistance)
+        private static Vector4 NewtonIteration(IntersectionData data, Vector4 xCur, Vector4 x0, float knotDistance, bool useCurrentTangent, Vector4 xTg)
         {
-            var mat = NewtonMatrix(data, xCur, x0);
-            var f = F(data, xCur, x0, knotDistance);
+            var mat = NewtonMatrix(data, xCur, useCurrentTangent ? xTg : x0);
+            var f = F(data, xCur, x0, knotDistance, useCurrentTangent, xTg);
             var dx = LinearEquationSolver.Solve(mat, -f);
 
             return dx + xCur;
         }
 
-        private static Vector4 F(IntersectionData data, Vector4 xCur, Vector4 x0, float knotDistance)
+        private static Vector4 F(IntersectionData data, Vector4 xCur, Vector4 x0, float knotDistance, bool useCurrentTangent, Vector4 xTg)
         {
             var p = data.p;
             var q = data.q;
@@ -153,12 +179,12 @@ namespace avoCADo.Algebra
             var t = xCur.W;
 
             var posDiff = p.GetVertex(u, v) - q.GetVertex(s, t);
-            var dir = StepDistance(data, xCur, x0, knotDistance);
+            var dir = StepDistance(data, xCur, x0, knotDistance, useCurrentTangent, xTg);
 
             return new Vector4(posDiff, dir);
         }
 
-        private static float StepDistance(IntersectionData data, Vector4 xCur, Vector4 x0, float knotDistance)
+        private static float StepDistance(IntersectionData data, Vector4 xCur, Vector4 x0, float knotDistance, bool useCurTangent, Vector4 xTg)
         {
             var p = data.p;
 
@@ -168,7 +194,7 @@ namespace avoCADo.Algebra
             var u0 = x0.X;
             var v0 = x0.Y;
 
-            return Vector3.Dot(p.GetVertex(u, v) - p.GetVertex(u0, v0), ICTangent(data, x0)) - knotDistance;
+            return Vector3.Dot(p.GetVertex(u, v) - p.GetVertex(u0, v0), ICTangent(data, useCurTangent?xTg:x0)) - knotDistance;
         }
 
         private static Vector3 ICTangent(IntersectionData data, Vector4 x)
@@ -176,6 +202,14 @@ namespace avoCADo.Algebra
             return Vector3.Cross(data.p.Normal(x.X, x.Y), data.q.Normal(x.Z, x.W)).Normalized();
         }
 
+
+        /// <summary>
+        /// x0 may be changed to (first) iteration of x that goes to next patch that turns in a sharp angle to previous patch (C0)
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="xCur"></param>
+        /// <param name="x0"></param>
+        /// <returns></returns>
         private static Matrix4 NewtonMatrix(IntersectionData data, Vector4 xCur, Vector4 x0)
         {
             //F(u,v,s,t) = P(u,v) - Q(s,t) [3 equations]
