4bbd58445a4a6c7035ccabf7dfd6e982d62977ec
MadDevX <eryk.dobrosz@gmail.com>
2020-10-31 21:00:30 +0100

    Tweak colors, refresh progress bar on select

--- avoCADo/MillableSurfaceView.xaml.cs
+++ avoCADo/MillableSurfaceView.xaml.cs
@@ -66,6 +66,7 @@ namespace avoCADo
                 SetStartButtonText();
                 UpdateToolInfo();
                 UpdateLoadedFilesText();
+                UpdateProgress();
             }
             else
             {
